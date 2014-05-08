4202b8d4de4ec2518ec5ceca9300efa23ab9fc64
Cleiton Gomes <cleitoncsg@gmail.com>
2014-05-08 13:19:13 -0300

    Laço em python

---
+++ dicionarioDados.py
@@ -0,0 +1,2 @@
+refeicoes = {"café": "pão de queijo", "almoço": "arroz", "jantar" : "sopa"}
+print refeicoes["almoço"]
---
+++ lacosEmPython.py
@@ -0,0 +1,5 @@
+for i in range(1,4):
+	print "%da volta" % i
+
+for indice in range(1,11):
+	print "\tTermo %d" % indice
